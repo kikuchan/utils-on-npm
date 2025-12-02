a6a6713c888d4e603ca40a5685f895260e1dc7ae
kikuchan <kikuchan98@gmail.com>
2025-12-02 20:16:48 +0900

    fix: build

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -8,20 +8,6 @@ const projectRoot = import.meta.dirname;
 const pkgName = pkg.name.replace(/^[^/]+\//, '');
 const outDir = path.join(projectRoot, 'dist', pkgName);
 
-function replaceRecursive(obj: object, replacer: (s: string, k: string) => string) {
-  const results = {};
-  for (const key in obj) {
-    if (typeof obj[key] === 'string') {
-      results[key] = replacer(obj[key], key);
-    } else if (typeof obj[key] === 'object') {
-      results[key] = replaceRecursive(obj[key], replacer);
-    } else {
-      results[key] = obj[key];
-    }
-  }
-  return results;
-}
-
 function replaceSrc(s: string, ext: string = 'js') {
   return s.replace(/^\.\/src\//, './').replace(/\.(vue|ts)$/, '.' + ext);
 }
@@ -31,6 +17,7 @@ export default defineConfig({
   dts: true,
 
   outDir,
+  fixedExtension: false,
 
   onSuccess() {
     fs.writeFileSync(
@@ -40,16 +27,15 @@ export default defineConfig({
           ...pkg,
           main: replaceSrc(pkg.main),
           types: replaceSrc(pkg.main, 'd.ts'),
-          exports: replaceRecursive(pkg.exports ?? { '.': replaceSrc(pkg.main) }, replaceSrc),
-          /*
-          dependencies: replaceRecursive(pkg.dependencies, (s, k) => {
-            if (s !== 'workspace:*') return s;
-
-            const ppkg = JSON.parse(fs.readFileSync(path.join(projectRoot, 'packages', k, 'package.json'), 'utf-8'));
-            if (ppkg?.version) return `^${ppkg.version}`;
-            return '*';
-          }),
-          */
+          exports: {
+            '.': {
+              types: replaceSrc(pkg.main, 'd.ts'),
+              import: replaceSrc(pkg.main),
+            },
+          },
+          repository: {
+            url: 'https://github.com/kikuchan/utils-on-npm',
+          },
           devDependencies: undefined,
           scripts: undefined,
           private: undefined,
