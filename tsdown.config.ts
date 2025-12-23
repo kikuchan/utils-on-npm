0670930c0f54f08944b1dc9d37b7e24994bbd1d5
kikuchan <kikuchan98@gmail.com>
2025-12-23 15:09:11 +0900

    fix: build scripts

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -5,11 +5,31 @@ import { defineConfig } from 'tsdown';
 const pkg = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
 const projectRoot = import.meta.dirname;
 
+const root = import.meta.dirname;
 const pkgName = pkg.name.replace(/^[^/]+\//, '');
 const outDir = path.join(projectRoot, 'dist', pkgName);
 
 function replaceSrc(s: string, ext: string = 'js') {
   return s.replace(/^\.\/src\//, './').replace(/\.(vue|ts)$/, '.' + ext);
+}
+
+function replaceRecursive(
+  obj: Record<string, unknown> | undefined,
+  replacer: (s: string, k: string) => string | undefined,
+) {
+  if (!obj) return undefined;
+
+  const results: Record<string, unknown> = {};
+  for (const key in obj) {
+    if (typeof obj[key] === 'string') {
+      results[key] = replacer(obj[key], key);
+    } else if (typeof obj[key] === 'object') {
+      results[key] = replaceRecursive(obj[key] as Record<string, unknown>, replacer);
+    } else {
+      results[key] = obj[key];
+    }
+  }
+  return results;
 }
 
 export default defineConfig({
@@ -40,6 +60,15 @@ export default defineConfig({
             url: 'https://github.com/kikuchan/utils-on-npm',
           },
           devDependencies: undefined,
+          dependencies: replaceRecursive(pkg.dependencies, (s, k) => {
+            if (s !== 'workspace:*') return s;
+
+            const ppkg = JSON.parse(
+              fs.readFileSync(path.join(root, 'packages', path.basename(k), 'package.json'), 'utf-8'),
+            );
+            if (ppkg?.version) return `^${ppkg.version}`;
+            return '*';
+          }),
           scripts: undefined,
           private: undefined,
         },
